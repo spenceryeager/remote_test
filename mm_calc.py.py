7aa871326952f069afd762756c78a208d022f35b
Spencer Yeager <36802222+spenceryeager@users.noreply.github.com>
2022-04-28 10:10:40 -0700

    Delete mm_calc.py.py

--- mm_calc.py.py
+++
@@ -1,8 +0,0 @@
-# amount of tetrabutylammonium hexafluorophosphate needed
-
-molarMass = 387.43 # g/mol
-concentration = 0.5 # mol/L
-volume = 0.1 # L
-
-grams = (molarMass * concentration * volume)
-print(grams, "grams needed")
